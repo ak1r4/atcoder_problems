880d24d2607964c52dfdc41c52cb40318cea7eb2
ak1r4 <ak1r4.j@gmail.com>
2021-01-03 09:25:33 +0000

    I - dp

--- dp/i.cpp
+++ dp/i.cpp
@@ -1,40 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-//
-//    H       T
-//  H   T   H   T
-// H T H T H T H T
-// 3 2 2 1 2 1 1 0
-
-
-void bt(vector<long double>& ans, long double prev_probs, int n_heads, int cur, const vector<long double>& probs, const int& min_n_heads) {
-    if (cur >= probs.size()) {
-        if (n_heads >= min_n_heads) {
-            ans.push_back(prev_probs);
-        }
-        return;
-    }
-
-    bt(ans, prev_probs * probs[cur], n_heads + 1, cur + 1, probs, min_n_heads);
-    bt(ans, prev_probs * (1 - probs[cur]), n_heads, cur + 1, probs, min_n_heads);
-}
-
 int main() {
     int n;
     cin >> n;
-    vector<long double> probs(n);
+    vector<double> probs(n);
     for (auto& p : probs) { cin >> p; }
 
-    vector<long double> ans;
-    bt(ans, 1.0, 0, 0, probs, probs.size() / 2 + 1);
+    // dp[i][j] - prob after tossed i coins with j heads
+    vector<vector<double>> dp(n+1, vector<double>(n+1, 0.0));
+    dp[0][0] = 1.0;
 
-    long double result = 0.0;
-    for (const auto& a : ans) {
-        result += a;
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 0; j <=i; ++j) {
+            dp[i][j] = dp[i-1][j-1] * probs[i-1] + dp[i-1][j] * (1- probs[i-1]);
+        }
     }
 
-    cout << result << "\n";
+    double ans = 0.0;
+
+    for (int i = 0; i <= n; ++i) {
+        if (i > n-i) {
+            ans += dp[n][i];
+        }
+    }
+
+
+    // the problem asks for 1e-9 precision
+    printf("%.10f\n", ans);
 
     return 0;
 }
